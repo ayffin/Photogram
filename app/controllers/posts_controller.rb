731847e216c580f23f9d03c7d96e1bc88fb741fd
aifi <ayffin@gmail.com>
2017-10-30 21:03:33 -0400

    added styles to root page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create(post_params)
+
+    if @post.save
+      flash[:notice] = "Image is uploaded successfully"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error saving image"
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
